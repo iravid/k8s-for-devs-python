8790dc9f83be3bddbf0aaf3e07b55602d45e4748
Itamar Ravid <iravid@iravid.com>
2020-09-15 10:59:36 +0300

    Configmaps

--- kubernetes_workshop/app.py
+++ kubernetes_workshop/app.py
@@ -20,3 +20,17 @@ def ready():
 def name():
     return f'Hello from {os.getenv("POD_NAME")}\n'
 
+@app.route('/print-env')
+def print_env():
+    return f'Env var value: {os.getenv("ENABLE_FEATURE")}\n'
+
+@app.route('/print-envfrom')
+def print_envfrom():
+    values = [f'{k}: {v}' for (k, v) in os.environ.items() if k.startswith('ENV_FROM_')]
+    return f'Env from values: {values}\n'
+
+@app.route('/print-file')
+def print_file():
+    contents = [line for line in open("/app/config/APPCONF")]
+    return f'Data from file: {contents}\n'
+
