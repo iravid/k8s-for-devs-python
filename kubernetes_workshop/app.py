ea183e77851f2ba3046f066ba5af0d5d22984c9b
Itamar Ravid <iravid@iravid.com>
2020-09-15 09:54:47 +0300

    Flaky liveness probe

--- kubernetes_workshop/app.py
+++ kubernetes_workshop/app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
     return 'Hello Kubernetes Workshop!'
+
+@app.route('/health')
+def health():
+    if random.randint(0, 100) <= 50:
+        raise Exception("Boom")
+    else:
+        return 'OK'
